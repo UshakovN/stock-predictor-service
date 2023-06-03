88ff562854e3122c37c6894a241fac1fdbefbe58
UshakovN <ushakovnika@mpei.ru>
2023-06-03 13:07:24 +0300

    move polygon fetcher with interface to fetcher file

--- data-fetcher/internal/fetcher/fetcher.go
+++ data-fetcher/internal/fetcher/fetcher.go
@@ -1,1 +1,64 @@
 package fetcher
+
+import (
+  "context"
+  "main/internal/queue"
+  "main/internal/storage"
+  "sync"
+
+  "github.com/UshakovN/stock-predictor-service/httpclient"
+)
+
+type Fetcher interface {
+  ContinuouslyFetch()
+  SaveFetcherState()
+  SetTickerId(tickerId string)
+}
+
+type fetcher struct {
+  ctx      context.Context
+  client   httpclient.HttpClient
+  storage  storage.Storage
+  msQueue  queue.MediaServiceQueue
+  state    *state
+  once     *sync.Once
+  tickerId string
+}
+
+func NewFetcher(ctx context.Context, config *Config) (Fetcher, error) {
+  client := httpclient.NewClient(
+    httpclient.WithContext(ctx),
+    httpclient.WithQueryApiToken(
+      apiTokenKey,
+      config.ApiToken,
+    ),
+    httpclient.WithRequestsLimit(
+      polygonReqsLimit,
+      polygonReqPerDur,
+      polygonWaitDur,
+      polygonDeadlineDur,
+    ))
+
+  fetcherState := newFetcherState(
+    config.ModeTotalHours,
+    config.ModeCurrentHours,
+  )
+
+  fetcherStorage, err := storage.NewStorage(ctx, config.StorageConfig)
+  if err != nil {
+    return nil, err
+  }
+  msQueue, err := queue.NewMediaServiceQueue(ctx, config.QueueConfig)
+  if err != nil {
+    return nil, err
+  }
+
+  return &fetcher{
+    ctx:     ctx,
+    client:  client,
+    storage: fetcherStorage,
+    msQueue: msQueue,
+    state:   fetcherState,
+    once:    &sync.Once{},
+  }, nil
+}
--- data-fetcher/internal/fetcher/state.go
+++ data-fetcher/internal/fetcher/state.go
@@ -1,72 +1,13 @@
 package fetcher
 
 import (
-  "context"
   "fmt"
   "main/internal/domain"
-  "main/internal/queue"
-  "main/internal/storage"
-  "sync"
   "time"
 
-  "github.com/UshakovN/stock-predictor-service/httpclient"
   "github.com/UshakovN/stock-predictor-service/utils"
   log "github.com/sirupsen/logrus"
 )
-
-type Fetcher interface {
-  ContinuouslyFetch()
-  SaveFetcherState()
-  SetTickerId(tickerId string)
-}
-
-type fetcher struct {
-  ctx      context.Context
-  client   httpclient.HttpClient
-  storage  storage.Storage
-  msQueue  queue.MediaServiceQueue
-  state    *state
-  once     *sync.Once
-  tickerId string
-}
-
-func NewFetcher(ctx context.Context, config *Config) (Fetcher, error) {
-  client := httpclient.NewClient(
-    httpclient.WithContext(ctx),
-    httpclient.WithQueryApiToken(
-      apiTokenKey,
-      config.ApiToken,
-    ),
-    httpclient.WithRequestsLimit(
-      polygonReqsLimit,
-      polygonReqPerDur,
-      polygonWaitDur,
-      polygonDeadlineDur,
-    ))
-
-  fetcherState := newFetcherState(
-    config.ModeTotalHours,
-    config.ModeCurrentHours,
-  )
-
-  fetcherStorage, err := storage.NewStorage(ctx, config.StorageConfig)
-  if err != nil {
-    return nil, err
-  }
-  msQueue, err := queue.NewMediaServiceQueue(ctx, config.QueueConfig)
-  if err != nil {
-    return nil, err
-  }
-
-  return &fetcher{
-    ctx:     ctx,
-    client:  client,
-    storage: fetcherStorage,
-    msQueue: msQueue,
-    state:   fetcherState,
-    once:    &sync.Once{},
-  }, nil
-}
 
 type state struct {
   finished         bool
